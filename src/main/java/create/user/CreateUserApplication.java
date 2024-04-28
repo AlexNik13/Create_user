a0121881256c56708f2f04d88117ce847a5d22f6
AlexNik <4pnav@gmail.com>
2024-04-28 08:38:52 +0300

    Initial commit

---
+++ src/main/java/create/user/CreateUserApplication.java
@@ -0,0 +1,14 @@
+package create.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("HideUtilityClassConstructor")
+@SpringBootApplication
+public class CreateUserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreateUserApplication.class, args);
+  }
+
+}
